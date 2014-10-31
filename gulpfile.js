05442edb0dcca9b55610f590b185fcbbabccb058
Brian Zelip <bzelip@gmail.com>
2014-10-31 14:52:57 -0500

    updates for class presentation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp     = require('gulp');
 var server   = require('gulp-webserver');
+var rename   = require('gulp-rename');
+var md       = require('gulp-remarkable');
+
+// build markdown files into html
+gulp.task('md', function() {
+  gulp.src('./*.md')
+    .pipe(md())
+    .pipe(rename(function (path) {
+      path.extname = '.html'
+    }))
+    .pipe(gulp.dest('./'));
+});
 
 // serve this to the browser via `$ gulp serve`
 gulp.task('serve', function() {
